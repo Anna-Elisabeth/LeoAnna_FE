e8bef1eb8c337b2e2385505fed9cd8fcce2697bf
Liliano-Balde <liliano.balde@hotmail.com>
2024-02-27 12:54:07 +0000

    migrated all the stuff from previous repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
-
+import Logo2 from './Logo2.png'
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <Router>
+        <nav className="navbar align-content-center " style={{ display: "flex", backgroundColor: "rgba(135,206,250, 0.50)", }} >
+        <Link to="/">
+            <img src={Logo2} alt='Logo Icon' className="navbar-brand" width="75px" />
+          </Link>
+          <Link to='/signIn'><button type="button" className="btn btn-dark" style={{ margin: "10px" }}>Sign In</button></Link>
+          <Link to='/basket'><img src="/icons8-cart-48.png"></img></Link>
+          <div>
+          <input placeholder='Search here'></input>
+          </div>
+        </nav>
+        <Routes>
+          <Route path='/' />
+          <Route path='/basket'/>
+          <Route path='/signIn'/>
+        </Routes>
+      </Router>
+ 
+    
   );
 }
-
+ 
 export default App;
+ 
