5f90204a05a11f732c49d3fc209dd5ffff2d9c54
Liliano-Balde <liliano.balde@hotmail.com>
2024-02-27 16:34:31 +0000

    "added bootstrap"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
-import Logo2 from './Logo2.png'
+import Logo2 from './Logo2.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -8,9 +9,9 @@ function App() {
     <Router>
       <nav className="navbar align-content-center " style={{ display: "flex", backgroundColor: "rgba(135,150,180, 0.10)", }} >
         <Link to="/">
-          <img src={Logo2} alt='Logo Icon' className="navbar-brand" width="140px" />
+          <img src={Logo2} alt='Logo Icon' className="navbar-brand" width="180px" />
         </Link>
-        <div><Link to='/signIn'><button type="button" className="btn btn-dark" style={{ margin: "15px" }}>Sign In</button></Link></div>
+        <div><Link to='/signIn'><button type="button" className="btn btn-primary btn-lg" style={{ margin: "15px" }}>Sign In</button></Link></div>
        <div><Link to='/basket'><img src="/icons8-cart-48.png" alt="" height={40}></img></Link></div>
         <div><input style={{ margin: "15px", width: "700px"}} placeholder='Search here'></input></div>
         
@@ -22,7 +23,7 @@ function App() {
       </Routes>
     </Router>
 
-    // adding text to push
+   
   );
 }
 
