977bd1fa8f6c83ea1ddc5a93461fe53f2ec16bbb
Craigm84 <craigmorris2907@gmail.com>
2024-02-27 16:03:17 +0000

    Updated Navbar

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Logo2 from './Logo2.png'
- 
+
 function App() {
   return (
-    
-      <Router>
-        <nav className="navbar align-content-center " style={{ display: "flex", backgroundColor: "rgba(135,206,250, 0.50)", }} >
+
+    <Router>
+      <nav className="navbar align-content-center " style={{ display: "flex", backgroundColor: "rgba(135,150,180, 0.10)", }} >
         <Link to="/">
-            <img src={Logo2} alt='Logo Icon' className="navbar-brand" width="75px" />
-          </Link>
-          <Link to='/signIn'><button type="button" className="btn btn-dark" style={{ margin: "10px" }}>Sign In</button></Link>
-          <Link to='/basket'><img src="/icons8-cart-48.png"></img></Link>
-          <div>
-          <input placeholder='Search here'></input>
-          </div>
-        </nav>
-        <Routes>
-          <Route path='/' />
-          <Route path='/basket'/>
-          <Route path='/signIn'/>
-        </Routes>
-      </Router>
- 
+          <img src={Logo2} alt='Logo Icon' className="navbar-brand" width="140px" />
+        </Link>
+        <div><Link to='/signIn'><button type="button" className="btn btn-dark" style={{ margin: "15px" }}>Sign In</button></Link></div>
+       <div><Link to='/basket'><img src="/icons8-cart-48.png" alt="" height={40}></img></Link></div>
+        <div><input style={{ margin: "15px", width: "700px"}} placeholder='Search here'></input></div>
+        
+      </nav>
+      <Routes>
+        <Route path='/' />
+        <Route path='/basket' />
+        <Route path='/signIn' />
+      </Routes>
+    </Router>
+
     // adding text to push
   );
 }
- 
+
 export default App;
- 
