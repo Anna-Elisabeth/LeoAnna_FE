56620aa8be89c97f4062f0e930577eca47475165
Liliano-Balde <liliano.balde@hotmail.com>
2024-02-28 11:01:06 +0000

    refactored navbar functioning

--- src/App.js
+++ src/App.js
@@ -2,29 +2,42 @@ import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Logo2 from './Logo2.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-
     <Router>
-      <nav className="navbar align-content-center " style={{ display: "flex", backgroundColor: "rgba(135,150,180, 0.10)", }} >
-        <Link to="/">
-          <img src={Logo2} alt='Logo Icon' className="navbar-brand" width="180px" />
-        </Link>
-        <div><Link to='/signIn'><button type="button" className="btn btn-primary btn-lg" style={{ margin: "15px" }}>Sign In</button></Link></div>
-       <div><Link to='/basket'><img src="/icons8-cart-48.png" alt="" height={40}></img></Link></div>
-        <div><input style={{ margin: "15px", width: "700px"}} placeholder='Search here'></input></div>
-        
-      </nav>
+      <Navbar className="navbar align-content-center " style={{ display: "flex", backgroundColor: "rgba(0, 0, 0, 0)" }} />
       <Routes>
         <Route path='/' />
-        <Route path='/basket' />
-        <Route path='/signIn' />
-      </Routes>
-    </Router>
+          <Route path='/cart' />
+          <Route path='/signIn' />  
+        </Routes>
+      </Router>
 
    
   );
 }
 
 export default App;
+
+
+
+
+
+{/* <Router>
+<nav className="navbar align-content-center " style={{ display: "flex", backgroundColor: "rgba(135,150,180, 0.10)", }} >
+  <Link to="/">
+    <img src={Logo2} alt='Logo Icon' className="navbar-brand" width="180px" />
+  </Link>
+  <div><Link to='/signIn'><button type="button" className="btn btn-primary btn-lg" style={{ margin: "15px" }}>Sign In</button></Link></div>
+ <div><Link to='/basket'><img src="/icons8-cart-48.png" alt="" height={40}></img></Link></div>
+  <div><input style={{ margin: "15px", width: "700px"}} placeholder='Search here'></input></div>
+  
+</nav>
+<Routes>
+  <Route path='/' />
+  <Route path='/basket' />
+  <Route path='/signIn' />
+</Routes>
+</Router> */}
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+
+function Navbar() {
+  return (
+    <>
+      <div className="main-navbar shadow-sm sticky-top">
+        <div className="top-navbar">
+          <div className="container-fluid">
+            <div className="row">
+              <div  className="col-md-2 my-auto d-none d-sm-none d-md-block d-lg-block">
+                <Link to="/">
+                <img src="/Logo2.png" alt="Logo Icon" className="navbar-brand" width="180px" />
+                </Link>
+              </div>
+              <div className="col-md-5 my-auto">
+                <form role="search">
+                  <div className="input-group">
+                    <input type="search" placeholder="Search your product" className="form-control" />
+                    <button className="btn bg-white" type="submit">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                      </svg>
+                    </button>
+                  </div>
+                </form>
+              </div>
+              <div className="col-md-5 my-auto">
+                <ul className="nav justify-content-end">
+                  <li className="nav-item">
+                    <button style={{color: "white"}} className="nav-link" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+                      </svg> (0)
+                    </button>
+                  </li>
+                  <li className="nav-item dropdown">       
+                  <div class="input-group flex-nowrap">  
+                  {/* <span style={{background: "rgba(0, 0, 0, 0)", border: "none", outline:"none", color: "white"}} class="input-group-text" id="addon-wrapping">Sign In</span>   */}
+                    <select  style={{ width: "120px", height: "37px", background: "rgba(0, 0, 0, 0)", border: "none", outline:"none", color: "white"}}>input type="text" class="form-contr
+                      <option style={{color: "white"}} >Sign in</option>
+                      <option style={{color: "blue"}} >Detached</option>
+                      <option style={{color: "blue"}}  > Semi-Detached</option>
+                      <option style={{color: "blue"}}  >Terraced</option>
+                      <option  style={{color: "blue"}} >Bungalow</option>
+                      <option style={{color: "blue"}} >Flat</option>
+                    </select>
+                    </div>         
+                    {/* <a style={{color: "white"}} className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li><a className="dropdown-item" href="#"><i className="fa fa-user"></i> Profile</a></li>
+                      <li><a className="dropdown-item" href="#"><i className="fa fa-list"></i> My Orders</a></li>
+                      <li><a className="dropdown-item" href="#"><i className="fa fa-shopping-cart"></i> My Cart</a></li>
+                      <li><a className="dropdown-item" href="#"><i className="fa fa-sign-out"></i> Logout</a></li>
+                    </ul>
+                    </a> */}
+                    
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
