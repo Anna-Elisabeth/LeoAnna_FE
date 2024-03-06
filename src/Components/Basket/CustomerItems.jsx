bc8e01919f3368b1cb157c03f584638e221c2319
Liliano-Balde <liliano.balde@hotmail.com>
2024-03-06 12:04:51 +0000

    totals, basket quantity all functional

--- src/Components/Basket/CustomerItems.jsx
+++ src/Components/Basket/CustomerItems.jsx
@@ -1,77 +1,65 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import ItemCard from "../Items/ItemCard";
+import Counter from "./Counter";
+import productImages from "../../productsImage.json";
+
 function Basket(props) {
     const params = useParams();
-    const [quantity, setQuantity] = useState("");
-    const [basket, setBasket] = useState("")
+    const [basket, setBasket] = useState([]);
+    
+
+    useEffect(() => {
+        getCustomer();
+    }, []);
+
     function getCustomer() {
         axios.get("http://localhost:8082/customer/get/" + params.id)
             .then((response) => setBasket(response.data.items))
-            .catch(console.log)
+            .catch(console.log);
     }
-    useEffect(() => { getCustomer() }, [])
- 
-    const basketItems = [];
-    for (const baskItems of basket) {
-        basketItems.push(
-            
-            
-            <div className="d-inline-flex " style={{  maxWidth: "20%", margin: "20px" }}>
-                <div className="card">      
-                    <div className="card-body">
-                        
-                        <h4>Item: {baskItems.name}</h4>
-                        <br></br><h6>Price:  £ {baskItems.price}</h6>
-                       
- 
- 
-                        <br></br><select className="form-control border-3 border-primary rounded" style={{ width: "250px" }} value={quantity.id} onChange={(e) => setQuantity(e.target.value)} required>
-                            <option value="">Select Quantity</option>
-                            <option value="0">0</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                            <option value="7">7</option>
-                            <option value="8">8</option>
-                            <option value="9">9</option>
-                        </select>
-                        <br></br><h6>Total: £</h6>
-                    </div>
+
+    function handleQuantityChange(index, newQuantity) {
+        const updatedBasket = [...basket];
+        updatedBasket[index].quantity = newQuantity;
+        setBasket(updatedBasket);
+    }
+
+    const basketItems = basket.map((basketItem, index) => (
+        
+        <div className="d-inline-flex" style={{ maxWidth: "20%", margin: "20px" }} key={index}>
+            <div className="card">
+                <div className="card-body">
+                    <img
+                        src={getImageUrl(basketItem.name)}
+                        alt="avatar"
+                        className="card-person"
+                        style={{ maxWidth: '100px', height: '100px' }}
+                    />
+                    <h5>{basketItem.name}</h5>
+                    <h8>Price: £ {basketItem.price}</h8>
+                    <Counter
+                        value={basketItem.quantity}
+                        onChange={(newQuantity) => handleQuantityChange(index, newQuantity)}
+                    />
+                    <h6>Total: £ {basketItem.price * (basketItem.quantity || 0)}</h6>
                 </div>
             </div>
-        )
+        </div>
+    ));
+
+    function getImageUrl(productName) {
+        const productNameLower = productName.toLowerCase();
+        return productImages[productNameLower] || "/default.png";
     }
- 
-   
-   
- 
+
     return (
-        
         <div>
-        <h1>Basket: </h1>
-        {basketItems}
-            {/* <div className="row">
-                <table className="table table-bordered" style={{ backgroundColor: "blue", color: "white", margin: "100px auto", textAlign: "center", marginSide: "15%" }}>
-                    <thead  >
-                        <tr>
-                            <th style={{ backgroundColor: '#2E43F1', color: "white" }}>Item</th>
-                            <th style={{ backgroundColor: '#2E43F1', color: "white" }}>Price</th>
-                            <th style={{ backgroundColor: '#2E43F1', color: "white" }}>Quanity</th>
-                            <th style={{ backgroundColor: '#2E43F1', color: "white" }}>Total</th>
-                        </tr>
-                    </thead>
-                    <tbody className="table-group-divider">
- 
-                    </tbody>
-                </table></div> */}
-              </div>);
- 
+            <h1>Basket: </h1>
+            {basketItems}
+        </div>
+    );
 }
- 
+
 export default Basket;
